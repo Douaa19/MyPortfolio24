b85adb91dbd4af731f893cc05a6911b95bbbfc37
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2024-06-06 19:38:29 +0100

    responsiveHeroHeader

--- src/components/layout/HeroHeader.js
+++ src/components/layout/HeroHeader.js
@@ -3,7 +3,7 @@ import ProfileImg from "../../assets/Profile.JPG";
 
 function HeroHeader() {
   return (
-    <div className="flex justify-center items-center gap-40 my-4 translate-y-36">
+    <div className="flex justify-center items-center md:gap-40 ssm:gap-20 my-4 translate-y-36">
       <div className="w-24 animate">
         <img
           src={ProfileImg}
@@ -12,8 +12,8 @@ function HeroHeader() {
         />
       </div>
       <div className="text-white uppercase text-center animate">
-        <h2 className="text-24 mb-3">larif douaa</h2>
-        <h1 className="text-28">full stack developer</h1>
+        <h2 className="md:text-24 ssm:text-18 mb-3">larif douaa</h2>
+        <h1 className="md:text-28 ssm:text-24">full stack developer</h1>
       </div>
     </div>
   );
