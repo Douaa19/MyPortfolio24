0b5b5c752327dd7abe8e0c2e243826558cd8cbbb
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2024-06-07 13:16:17 +0100

    useSectionStateToChangeTextColotLinks

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -13,7 +13,7 @@ function Header(props) {
 
   return (
     <div className="w-11/12 h-screen pt-2 flex flex-col items-center">
-      <NavBar setSection={props} />
+      <NavBar setSection={props} section={props.section} />
       {section}
     </div>
   );
