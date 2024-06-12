b5859a2e47012a7805fb8ead52391c0de30e4f9f
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2024-06-12 19:32:00 +0100

    projectsSlider

--- src/components/organismes/ProjectCard.js
+++ src/components/organismes/ProjectCard.js
@@ -1,18 +1,32 @@
 import React from "react";
 import ProjectImg from "../../assets/eren-yildiz-g6Wd6MS8lms-unsplash.jpg";
 
-function ProjectCard({ title, post, technologies, description, link, light }) {
+function ProjectCard({
+  title,
+  post,
+  technologies,
+  description,
+  link,
+  light,
+  transition,
+}) {
   return (
-    <div class={`rounded-lg border ${light ? `text-dark` : `text-white`}`}>
+    <div
+      class={`rounded-lg border ${light ? `text-dark` : `text-white`} ${
+        transition === true ? `card-item` : ``
+      } mx-4`}>
       <img
         src={ProjectImg}
         alt="project-img"
         class="w-full h-48 rounded-t-md object-cover"
       />
       <div class="p-4">
-        <h2 className="uppercase md:text-18 ssm:text-14 font-bold">{title}</h2>
-        <h3 className="uppercase md:text-16 ssm:text-12">{post}</h3>
+        <h2 className="uppercase md:text-18 ssm:text-14 font-bold">
+          title: {title}
+        </h2>
+        <h3 className="uppercase md:text-16 ssm:text-12">role: {post}</h3>
         <div className="flex flex-wrap flex-row gap-2">
+          technologies:
           {technologies.map((techno, index) => (
             <span
               className={`${
@@ -23,14 +37,14 @@ function ProjectCard({ title, post, technologies, description, link, light }) {
             </span>
           ))}
         </div>
-        <p class="md:text-14 ssm:text-12">{description}</p>
+        <p class="md:text-14 ssm:text-12">description: {description}</p>
         <div className="">
           <a
             href="/"
             className={`hover:text-main md:text-14 ssm:text-12 ${
               light ? `text-dark` : `text-white`
             }`}>
-            {link}
+            link: {link}
           </a>
         </div>
       </div>
--- src/components/templates/CardGrid.js
+++ src/components/templates/CardGrid.js
@@ -7,41 +7,57 @@ import { ReactComponent as Prev } from "../../assets/arrow-prev-small-svgrepo-co
 function CardGrid(props) {
   const myArr = [1, 2, 3, 4, 5, 6];
   const settings = {
-    dots: true,
+    dots: false,
     Infinite: true,
     speed: 500,
     autoplay: true,
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1,
     nextArrow: <Next />,
     prevArrow: <Prev />,
-    // useTransform: true,
-    // responsive: [
-    //   {
-    //     breakpoint: 1200,
-    //     settings: {
-    //       slidesToShow: 3,
-    //       slidesToScroll: 3,
-    //       infinite: true,
-    //       dots: true,
-    //     },
-    //   },
-    //   {
-    //     breakpoint: 800,
-    //     settings: {
-    //       slidesToShow: 2,
-    //       slidesToScroll: 2,
-    //       initialSlide: 2,
-    //     },
-    //   },
-    // ],
+    useTransform: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+    ],
   };
 
   return (
-    <div className="w-[90%]">
+    <div className="w-full">
       <div class="flex items-center justify-center">
         <div class="container mx-auto p-4">
-          <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-4">
+          <Slider {...settings}>
+            {props.projects.map((project, index) => (
+              <>
+                <ProjectCard
+                  key={index}
+                  title={project.title}
+                  post={project.post}
+                  technologies={project.technologies}
+                  description={project.description}
+                  link={project.link}
+                  light={props.light}
+                  tansition={false}
+                />
+              </>
+            ))}
+          </Slider>
+          {/* <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-4">
             {props.projects.map((project, index) => (
               <ProjectCard
                 key={index}
@@ -51,9 +67,10 @@ function CardGrid(props) {
                 description={project.description}
                 link={project.link}
                 light={props.light}
+                tansition={false}
               />
             ))}
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
